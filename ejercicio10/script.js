7249cb9c366463450c66bc41037e69fef60a0036
AgustinPinto <aguspinto08@gmail.com>
2025-04-21 16:49:13 -0300

    script.js

--- ejercicio10/script.js
+++ ejercicio10/script.js
@@ -1,13 +1,13 @@
 function boton(){
-    var dado1, dado2, result, array = [0,0,0,0,0,0,0,0,0,0,0]
+    var dado1, dado2, result, array = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 
     for(i=0; i<36000; i++){
-        dado1 = parseInt(Math.random() * 7)
-        dado2 = parseInt(Math.random() * 7)
+        dado1 = parseInt(Math.random() * 6)-1
+        dado2 = parseInt(Math.random() * 6)-1
         result = dado1 + dado2
-        array[result-2]++
+        array[result]++
     }
     
     for(r=2; r<=12; r++)
-        console.log(r+":"+array[r-2])
+        console.log(r+":"+array[r])
 }
