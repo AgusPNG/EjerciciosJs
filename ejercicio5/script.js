842ec9ad43dd9e91aacc69313826f6addff9dced
AgustinPinto <aguspinto08@gmail.com>
2025-04-21 16:51:44 -0300

    script.js

--- ejercicio5/script.js
+++ ejercicio5/script.js
@@ -1,10 +1,9 @@
 function boton(){
     let num = parseInt(prompt("Introduzca un numero"))
-    let x = 0, y = num
+    let y = num
 
-    for(let n=(num-1); n>0; n--){
-        x = y * n
-        y = x
-    }
+    for(let n=(num-1); n>0; n--)
+        y *= n
+        
     console.log("El resultado del factorial de "+num+" es "+y)
 }
