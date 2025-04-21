3a88a6d0deb41cf2dd5e071bc51c32b1fb727dac
AgustinPinto <aguspinto08@gmail.com>
2025-04-21 16:51:02 -0300

    script.js

--- ejercicio2/script.js
+++ ejercicio2/script.js
@@ -1,17 +1,15 @@
 function boton(){
     var valores = [true, 5, false, "hola", "adios",2];
-    var booleano = valores[0]
 
     if(valores[3] > valores[4])
         console.log("La palabra mayor es "+ valores[3]);
-    
     else
         console.log("La palabra mayor es "+ valores[4]);
 
-    if(booleano == valores[0])
+    if(valores[0] == true)
         console.log("El resultado es "+valores[0])
 
-    if(booleano == valores[2])
+    if(valores[2] == false)
         console.log("El resultado es "+valores[2])
 
     console.log(valores[1]+"+"+valores[5]+"="+(valores[1]+valores[5]))
