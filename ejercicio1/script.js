ad4993716898d636a284f7b2a08797ed190965c8
AgustinPinto <aguspinto08@gmail.com>
2025-04-21 16:48:10 -0300

    script.js

--- ejercicio1/script.js
+++ ejercicio1/script.js
@@ -1,6 +1,6 @@
 function boton(){
     let arr = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
 
-    for(let i=0; i<=12; i++)
+    for(let i=0; i<12; i++)
         console.log(arr[i])
 }
